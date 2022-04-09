94b1756af1a8e089e900100ce66460b287a3a654
chao zhao <cszhaochao@gmail.com>
2022-04-09 18:58:28 -0400

    finished main functions and runs well

--- app.py
+++ app.py
@@ -8,32 +8,8 @@ import plotly.graph_objects as go
 import json
 
 app = Flask(__name__)
+history = []
 
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/boxplot'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = True
-#
-#
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-#
-# class Moody(db.Model):
-#     __tablename__ = 'moody2022'
-#     score = db.Column(db.Float, primary_key=True)
-#     grade = db.Column(db.String)
-#     dozes_off = db.Column(db.String)
-#     texting_in_class = db.Column(db.String)
-#     participation = db.Column(db.Float)
-#
-# def get_data():
-#     try:
-#         socks = Moody.query.filter_by(dozes_off='never').order_by(Moody.score).all()
-#
-#         return
-#     except Exception as e:
-#         print(e)
-#
 def get_plot(data):
     fig = go.Figure()
     if not data:
@@ -43,11 +19,7 @@ def get_plot(data):
     fig.add_trace(go.Box(y=data[2], name='C', marker_color='indianred'))
     fig.add_trace(go.Box(y=data[3], name='D', marker_color='indianred'))
     fig.add_trace(go.Box(y=data[4], name='F', marker_color='indianred'))
-    # fig.update_layout(title_text="Distribution of Scores by Grade for all")
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-    # fig.show()
-
 
 def get_all_data():
     engine = create_engine('mysql+pymysql://root:root@localhost:3306/boxplot', max_overflow=10)
@@ -100,54 +72,27 @@ def get_query_data(s):
             score[i] = float(score[i][0])
     return scores
 
+@app.route("/", methods=['GET'])
+def welcome():
+    return render_template("welcome.html")
 
-# def get_query_data(s):
-#     engine = create_engine('mysql+pymysql://root:root@localhost:3306/boxplot', max_overflow=10)
-#     s = s.lower()
-#     first_condition = " where "
-#     base_sql = "select score from moody2022 "
-#     for i in range(len(s) - 5):
-#         if(s[i:i+5] == 'where'):
-#             first_condition = s[i:]
-#     condition = "GRADE = '"
-#     grades=['A', 'B', 'C', 'D', 'F']
-#     if first_condition == " where ":
-#         return {}
-#     scores = {}
-#     for i in range(5):
-#         sql = base_sql + first_condition + ' and ' + condition + grades[i] + "'"
-#         scores[grades[i]] = engine.execute(sql).fetchall()
-#     for score in scores.values():
-#         for i in range(len(score)):
-#             score[i] = float(score[i][0])
-#     return scores
-
-# cur = engine.execute(sql)
-# return str(cur.fetchall())
-# result = cur.fetchall()
-# res = '<ul>'
-# for a in result:
-#     for b in a:
-#         res = res + '<li>'
-#         res = res + str(b)
-#         res = res + '</li>'
-# res = res + '</ul>'
-# return res
-
-@app.route("/", methods=['GET','POST'])
+@app.route("/index", methods=['POST', 'GET'])
 def start():
-    s = request.form.get("sql_query")
-    data = get_all_data()
-    query_data = get_query_data(s)
-    return render_template('index.html', graph1=get_plot(data), graph2=get_plot(query_data))
+    try:
+        s = request.form.get("sql_query")
+        history.append(s)
+        if len(history) > 20:
+            del history[0]
+        data = get_all_data()
+        query_data = get_query_data(s)
+        return render_template('index.html', graph1=get_plot(data), graph2=get_plot(query_data))
+    except:
+        return render_template('fail.html')
 
+@app.route("/history", methods=['POST', 'GET'])
+def queryHistory():
+    return render_template('history.html', his = history)
 
 if __name__ == '__main__':
     app.run()
-    # res = get_all_data()
-    # get_plot(res)
-    # s = "select * from moody2022 where dozes_off='never' or participation>0.1"
-    # rr = get_query_data(s)
-    # get_plot(rr)
-    # print(rr)
-    # str = "SELECT * FROM moody2022_new WHERE "
+
