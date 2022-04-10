32a3159110403d28ed7f68e22f6b9fa74cdd7a90
chao zhao <cszhaochao@gmail.com>
2022-04-10 14:47:21 -0400

    update the plot

--- app.py
+++ app.py
@@ -10,19 +10,49 @@ import json
 app = Flask(__name__)
 history = []
 
-def get_plot(data):
+
+def get_plot(data, queried_data):
     fig = go.Figure()
-    if not data:
-        return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    fig.add_trace(go.Box(y=data[0], name='A', marker_color='indianred'))
-    fig.add_trace(go.Box(y=data[1], name='B', marker_color='indianred'))
-    fig.add_trace(go.Box(y=data[2], name='C', marker_color='indianred'))
-    fig.add_trace(go.Box(y=data[3], name='D', marker_color='indianred'))
-    fig.add_trace(go.Box(y=data[4], name='F', marker_color='indianred'))
+    if not queried_data:
+        fig.add_trace(go.Box(y=data[0], name='A', marker_color='indianred'))
+        fig.add_trace(go.Box(y=data[1], name='B', marker_color='indianred'))
+        fig.add_trace(go.Box(y=data[2], name='C', marker_color='indianred'))
+        fig.add_trace(go.Box(y=data[3], name='D', marker_color='indianred'))
+        fig.add_trace(go.Box(y=data[4], name='F', marker_color='indianred'))
+    else:
+        x1, x2 = [], []
+        data1, data2 = [], []
+        tag = ['A', 'B', 'C', 'D', 'F']
+        for i in range(5):
+            for j in range(len(data[i])):
+                x1.append(tag[i])
+                data1.append(data[i][j])
+            for k in range(len(queried_data[i])):
+                x2.append(tag[i])
+                data2.append(queried_data[i][k])
+        fig.add_trace(go.Box(
+            y=data1,
+            x=x1,
+            name='Boxplot Zero',
+            marker_color='#3D9970'
+        ))
+        fig.add_trace(go.Box(
+            y=data2,
+            x=x2,
+            name='Slice Boxplot',
+            marker_color='#FF4136'
+        ))
+
+        fig.update_layout(
+            yaxis_title='Score',
+            boxmode='group'
+        )
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
+
 def get_all_data():
-    engine = create_engine('mysql+pymysql://admin:root1234@boxplot.co5oyjgpvi8x.us-east-1.rds.amazonaws.com:3306/boxplot', max_overflow=10)
+    engine = create_engine(
+        'mysql+pymysql://admin:root1234@boxplot.co5oyjgpvi8x.us-east-1.rds.amazonaws.com:3306/boxplot', max_overflow=10)
     base_sql = "select score from moody2022_new where "
     conditions = []
     conditions.append("GRADE = 'A' ")
@@ -42,8 +72,9 @@ def get_all_data():
 
 def get_query_data(s):
     if not s:
-        return[]
-    engine = create_engine('mysql+pymysql://admin:root1234@boxplot.co5oyjgpvi8x.us-east-1.rds.amazonaws.com:3306/boxplot', max_overflow=10)
+        return []
+    engine = create_engine(
+        'mysql+pymysql://admin:root1234@boxplot.co5oyjgpvi8x.us-east-1.rds.amazonaws.com:3306/boxplot', max_overflow=10)
     s = s.lower()
     first_condition = " where "
     base_sql = "select score from moody2022_new "
@@ -72,9 +103,11 @@ def get_query_data(s):
             score[i] = float(score[i][0])
     return scores
 
+
 @app.route("/", methods=['GET'])
 def welcome():
     return render_template("welcome.html")
+
 
 @app.route("/index", methods=['POST', 'GET'])
 def start():
@@ -85,14 +118,15 @@ def start():
             del history[0]
         data = get_all_data()
         query_data = get_query_data(s)
-        return render_template('index.html', graph1=get_plot(data), graph2=get_plot(query_data), query_sentence=s)
+        return render_template('index.html', graph=get_plot(data, query_data), query_sentence=s)
     except:
         return render_template('fail.html')
 
+
 @app.route("/history", methods=['POST', 'GET'])
 def queryHistory():
-    return render_template('history.html', his = history)
+    return render_template('history.html', his=history)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
